09015dd427c953dd9e1bfd8be0dd8a6a7eb018c9
alainluthier <alainluthier@gmail.com>
2021-05-11 17:09:22 -0400

    new

--- accounts/index.php
+++ accounts/index.php
@@ -1,16 +1,13 @@
 <?php
-ini_set( 'error_reporting', E_ALL );
-ini_set( 'display_errors', true );
+//Accounts Controller
+
 // Get the database connection file
-require_once 'library/connections.php';
+require_once '../library/connections.php';
 // Get the PHP Motors model for use as needed
-require_once 'model/main-model.php';
+require_once '../model/main-model.php';
 
 // Get the array of classifications
 $classifications = getClassifications();
-
-//var_dump($classifications);
-//exit;
     
 // Build a navigation bar using the $classifications array
 $navList = '<ul>';
@@ -20,18 +17,14 @@ foreach ($classifications as $classification) {
 }
 $navList .= '</ul>';
 
-//echo $navList;
-//exit;
 
 $action = filter_input(INPUT_POST, 'action');
 if ($action == NULL) {
     $action = filter_input(INPUT_GET, 'action');
 }
 switch ($action) {
-    case 'template':
-        include 'view/template.php';
+    case '':
         break;
     default:
-        include 'view/home.php';
         break;
 }
