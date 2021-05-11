40f6fb5cfa63746d0355523d564fae09bd048c26
alainluthier <alainluthier@gmail.com>
2021-05-11 14:15:04 -0400

    test

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
 <?php
+ini_set( 'error_reporting', E_ALL );
+ini_set( 'display_errors', true );
 $action = filter_input(INPUT_POST, 'action');
 if ($action == NULL) {
     $action = filter_input(INPUT_GET, 'action');
@@ -7,6 +9,6 @@ switch ($action) {
     case 'something':
         break;
     default:
+        echo 'test';
         include 'view/home.php';
 }
-?>
