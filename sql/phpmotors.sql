3ce9cfdfff1d519de35ccd552e25b7706b9f3eaa
alainluthier <alainluthier@gmail.com>
2021-07-16 22:38:09 -0400

    final version

--- sql/phpmotors.sql
+++ sql/phpmotors.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.0
+-- version 5.0.2
 -- https://www.phpmyadmin.net/
 --
--- Servidor: localhost
--- Tiempo de generación: 03-07-2021 a las 07:44:43
--- Versión del servidor: 10.3.27-MariaDB-0+deb10u1
--- Versión de PHP: 7.3.27-1~deb10u1
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 17-07-2021 a las 04:37:32
+-- Versión del servidor: 10.4.14-MariaDB
+-- Versión de PHP: 7.4.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -27,6 +27,7 @@ SET time_zone = "+00:00";
 -- Estructura de tabla para la tabla `carclassification`
 --
 
+DROP TABLE IF EXISTS `carclassification`;
 CREATE TABLE `carclassification` (
   `classificationId` int(11) NOT NULL,
   `classificationName` varchar(30) NOT NULL
@@ -41,7 +42,8 @@ INSERT INTO `carclassification` (`classificationId`, `classificationName`) VALUE
 (2, 'Classic'),
 (3, 'Sports'),
 (4, 'Trucks'),
-(5, 'Used');
+(5, 'Used'),
+(14, 'Lemons');
 
 -- --------------------------------------------------------
 
@@ -49,6 +51,7 @@ INSERT INTO `carclassification` (`classificationId`, `classificationName`) VALUE
 -- Estructura de tabla para la tabla `clients`
 --
 
+DROP TABLE IF EXISTS `clients`;
 CREATE TABLE `clients` (
   `clientId` int(10) UNSIGNED NOT NULL,
   `clientFirstname` varchar(15) NOT NULL,
@@ -64,8 +67,9 @@ CREATE TABLE `clients` (
 --
 
 INSERT INTO `clients` (`clientId`, `clientFirstname`, `clientLastname`, `clientEmail`, `clientPassword`, `clientLevel`, `comment`) VALUES
-(1, 'Admin', 'Administrator', 'admin@cse340.net', '$2y$10$hg9EhPlL1GmyoE8TdU6CMOLBsBdRIcB5F/huEZ6sfgi1D2unZUAFq', '3', NULL),
-(2, 'Adam', 'Hansen', 'adam@test.com', '$2y$10$HZUr1XW7NeCKSdHKLjrLMeGWbvqDrmQ7xdPmi9d5OdCDwoBX9uqFC', '1', NULL);
+(1, 'Alain', 'Ramos', 'alainluthier@gmail.com', '$2y$10$JTQSbTXKHFNNV4MbAZ2g5Oy0ODDSQZR6PBwKn/Su.llBRc29kUGw2', '1', NULL),
+(2, 'Adam', 'Smith', 'adam@test.com', '$2y$10$kHV0yLZXi/N2q7MGfc/g2.KptlfXqlE7sYmHhF/c5Qtidymn/sEJq', '1', NULL),
+(3, 'Administrator', 'User', 'admin@cse340.net', '$2y$10$JTQSbTXKHFNNV4MbAZ2g5Oy0ODDSQZR6PBwKn/Su.llBRc29kUGw2', '3', NULL);
 
 -- --------------------------------------------------------
 
@@ -73,6 +77,7 @@ INSERT INTO `clients` (`clientId`, `clientFirstname`, `clientLastname`, `clientE
 -- Estructura de tabla para la tabla `images`
 --
 
+DROP TABLE IF EXISTS `images`;
 CREATE TABLE `images` (
   `imgId` int(11) NOT NULL,
   `invId` int(11) NOT NULL,
@@ -87,8 +92,44 @@ CREATE TABLE `images` (
 --
 
 INSERT INTO `images` (`imgId`, `invId`, `imgName`, `imgPath`, `imgDate`, `imgPrimary`) VALUES
-(3, 2, 'vive_sin_violencia.jpg', '/phpmotors/uploads/images/vive_sin_violencia.jpg', '2021-06-28 17:38:38', 0),
-(4, 2, 'vive_sin_violencia-tn.jpg', '/phpmotors/uploads/images/vive_sin_violencia-tn.jpg', '2021-06-28 17:38:39', 0);
+(17, 1, 'wrangler.jpg', '/phpmotors/images/vehicles/wrangler.jpg', '2021-07-04 02:49:45', 1),
+(18, 1, 'wrangler-tn.jpg', '/phpmotors/images/vehicles/wrangler-tn.jpg', '2021-07-04 02:49:45', 1),
+(19, 2, 'model-t.jpg', '/phpmotors/images/vehicles/model-t.jpg', '2021-07-04 02:50:06', 1),
+(20, 2, 'model-t-tn.jpg', '/phpmotors/images/vehicles/model-t-tn.jpg', '2021-07-04 02:50:06', 1),
+(21, 3, 'adventador.jpg', '/phpmotors/images/vehicles/adventador.jpg', '2021-07-04 02:50:16', 1),
+(22, 3, 'adventador-tn.jpg', '/phpmotors/images/vehicles/adventador-tn.jpg', '2021-07-04 02:50:16', 1),
+(23, 4, 'monster-truck.jpg', '/phpmotors/images/vehicles/monster-truck.jpg', '2021-07-04 02:50:27', 1),
+(24, 4, 'monster-truck-tn.jpg', '/phpmotors/images/vehicles/monster-truck-tn.jpg', '2021-07-04 02:50:27', 1),
+(25, 5, 'mechanic.jpg', '/phpmotors/images/vehicles/mechanic.jpg', '2021-07-04 02:50:45', 1),
+(26, 5, 'mechanic-tn.jpg', '/phpmotors/images/vehicles/mechanic-tn.jpg', '2021-07-04 02:50:45', 1),
+(27, 6, 'batmobile.jpg', '/phpmotors/images/vehicles/batmobile.jpg', '2021-07-04 02:50:58', 1),
+(28, 6, 'batmobile-tn.jpg', '/phpmotors/images/vehicles/batmobile-tn.jpg', '2021-07-04 02:50:58', 1),
+(29, 7, 'mystery-van.jpg', '/phpmotors/images/vehicles/mystery-van.jpg', '2021-07-04 02:51:18', 1),
+(30, 7, 'mystery-van-tn.jpg', '/phpmotors/images/vehicles/mystery-van-tn.jpg', '2021-07-04 02:51:18', 1),
+(31, 8, 'fire-truck.jpg', '/phpmotors/images/vehicles/fire-truck.jpg', '2021-07-04 02:51:32', 1),
+(32, 8, 'fire-truck-tn.jpg', '/phpmotors/images/vehicles/fire-truck-tn.jpg', '2021-07-04 02:51:32', 1),
+(33, 9, 'crwn-vic.jpg', '/phpmotors/images/vehicles/crwn-vic.jpg', '2021-07-04 02:51:45', 1),
+(34, 9, 'crwn-vic-tn.jpg', '/phpmotors/images/vehicles/crwn-vic-tn.jpg', '2021-07-04 02:51:45', 1),
+(35, 10, 'camaro.jpg', '/phpmotors/images/vehicles/camaro.jpg', '2021-07-04 02:51:58', 1),
+(36, 10, 'camaro-tn.jpg', '/phpmotors/images/vehicles/camaro-tn.jpg', '2021-07-04 02:51:58', 1),
+(37, 11, 'escalade.jpg', '/phpmotors/images/vehicles/escalade.jpg', '2021-07-04 02:52:15', 1),
+(38, 11, 'escalade-tn.jpg', '/phpmotors/images/vehicles/escalade-tn.jpg', '2021-07-04 02:52:15', 1),
+(39, 12, 'hummer.jpg', '/phpmotors/images/vehicles/hummer.jpg', '2021-07-04 02:52:29', 1),
+(40, 12, 'hummer-tn.jpg', '/phpmotors/images/vehicles/hummer-tn.jpg', '2021-07-04 02:52:29', 1),
+(41, 13, 'aerocar.jpg', '/phpmotors/images/vehicles/aerocar.jpg', '2021-07-04 02:53:00', 1),
+(42, 13, 'aerocar-tn.jpg', '/phpmotors/images/vehicles/aerocar-tn.jpg', '2021-07-04 02:53:00', 1),
+(43, 14, 'van.jpg', '/phpmotors/images/vehicles/van.jpg', '2021-07-04 02:53:23', 1),
+(44, 14, 'van-tn.jpg', '/phpmotors/images/vehicles/van-tn.jpg', '2021-07-04 02:53:23', 1),
+(45, 15, 'no-image.png', '/phpmotors/images/vehicles/no-image.png', '2021-07-04 02:53:35', 1),
+(46, 15, 'no-image-tn.png', '/phpmotors/images/vehicles/no-image-tn.png', '2021-07-04 02:53:35', 1),
+(47, 3, 'adventador2.jpg', '/phpmotors/images/vehicles/adventador2.jpg', '2021-07-04 04:01:33', 0),
+(48, 3, 'adventador2-tn.jpg', '/phpmotors/images/vehicles/adventador2-tn.jpg', '2021-07-04 04:01:33', 0),
+(49, 3, 'adventador3.jpg', '/phpmotors/images/vehicles/adventador3.jpg', '2021-07-04 04:02:01', 0),
+(50, 3, 'adventador3-tn.jpg', '/phpmotors/images/vehicles/adventador3-tn.jpg', '2021-07-04 04:02:01', 0),
+(51, 4, 'monster-truck2.jpg', '/phpmotors/images/vehicles/monster-truck2.jpg', '2021-07-04 04:03:36', 0),
+(52, 4, 'monster-truck2-tn.jpg', '/phpmotors/images/vehicles/monster-truck2-tn.jpg', '2021-07-04 04:03:36', 0),
+(53, 10, 'camaro2.jpg', '/phpmotors/images/vehicles/camaro2.jpg', '2021-07-04 04:03:52', 0),
+(54, 10, 'camaro2-tn.jpg', '/phpmotors/images/vehicles/camaro2-tn.jpg', '2021-07-04 04:03:52', 0);
 
 -- --------------------------------------------------------
 
@@ -96,6 +137,7 @@ INSERT INTO `images` (`imgId`, `invId`, `imgName`, `imgPath`, `imgDate`, `imgPri
 -- Estructura de tabla para la tabla `inventory`
 --
 
+DROP TABLE IF EXISTS `inventory`;
 CREATE TABLE `inventory` (
   `invId` int(11) NOT NULL,
   `invMake` varchar(30) NOT NULL,
@@ -118,7 +160,7 @@ INSERT INTO `inventory` (`invId`, `invMake`, `invModel`, `invDescription`, `invI
 (2, 'Ford', 'Model T', 'The Ford Model T can be a bit tricky to drive. It was the first car to be put into production. You can get it in any color you want as long as it\'s black.', '/phpmotors/images/model-t.jpg', '/phpmotors/images/model-t-tn.jpg', '30000', 2, 'Black', 2),
 (3, 'Lamborghini', 'Adventador', 'This V-12 engine packs a punch in this sporty car. Make sure you wear your seatbelt and obey all traffic laws. ', '/phpmotors/images/adventador.jpg', '/phpmotors/images/adventador-tn.jpg', '417650', 1, 'Blue', 3),
 (4, 'Monster', 'Truck', 'Most trucks are for working, this one is for fun. this beast comes with 60in tires giving you tracktions needed to jump and roll in the mud.', '/phpmotors/images/monster-truck.jpg', '/phpmotors/images/monster-truck-tn.jpg', '150000', 3, 'purple', 4),
-(5, 'Mechanic', 'Special', 'Not sure where this car came from. however with a little tlc it will run as good a new.', '/phpmotors/images/mechanic-van.jpg', '/phpmotors/images/mechanic-tn.jpg', '100', 200, 'Rust', 5),
+(5, 'Mechanic', 'Special', 'Not sure where this car came from. however with a little tlc it will run as good a new.', '/phpmotors/images/mechanic.jpg', '/phpmotors/images/mechanic-tn.jpg', '100', 200, 'Rust', 5),
 (6, 'Batmobile', 'Custom', 'Ever want to be a super hero? now you can with the batmobile. This car allows you to switch to bike mode allowing you to easily maneuver through trafic during rush hour.', '/phpmotors/images/batmobile.jpg', '/phpmotors/images/batmobile-tn.jpg', '65000', 2, 'Black', 3),
 (7, 'Mystery', 'Machine', 'Scooby and the gang always found luck in solving their mysteries because of there 4 wheel drive Mystery Machine. This Van will help you do whatever job you are required to with a success rate of 100%.', '/phpmotors/images/mystery-van.jpg', '/phpmotors/images/mystery-van-tn.jpg', '10000', 12, 'Green', 1),
 (8, 'Spartan', 'Fire Truck', 'Emergencies happen often. Be prepared with this Spartan fire truck. Comes complete with 1000 ft. of hose and a 1000 gallon tank.', '/phpmotors/images/fire-truck.jpg', '/phpmotors/images/fire-truck-tn.jpg', '50000', 2, 'Red', 4),
@@ -128,7 +170,31 @@ INSERT INTO `inventory` (`invId`, `invMake`, `invModel`, `invDescription`, `invI
 (12, 'GM', 'Hummer', 'Do you have 6 kids and like to go offroading? The Hummer gives you the small interiors with an engine to get you out of any muddy or rocky situation.', '/phpmotors/images/hummer.jpg', '/phpmotors/images/hummer-tn.jpg', '58800', 5, 'Yellow', 5),
 (13, 'Aerocar International', 'Aerocar', 'Are you sick of rushhour trafic? This car converts into an airplane to get you where you are going fast. Only 6 of these were made, get them while they last!', '/phpmotors/images/aerocar.jpg', '/phpmotors/images/aerocar-tn.jpg', '1000000', 6, 'Red', 2),
 (14, 'FBI', 'Survalence Van', 'do you like police shows? You\'ll feel right at home driving this van, come complete with survalence equipments for and extra fee of $2,000 a month. ', '/phpmotors/images/van.jpg', '/phpmotors/images/van-tn.jpg', '20000', 1, 'Green', 1),
-(15, 'Dog ', 'Car', 'Do you like dogs? Well this car is for you straight from the 90s from Aspen, Colorado we have the orginal Dog Car complete with fluffy ears.  ', '/phpmotors/images/no-image.png', '/phpmotors/images/no-image-tn.png', '35000', 1, 'Brown', 2);
+(15, 'Dog ', 'Car', 'Do you like dogs? Well this car is for you straight from the 90s from Aspen, Colorado we have the orginal Dog Car complete with fluffy ears.  ', '/phpmotors/images/crwn-vic.jpg', '/phpmotors/images/crwn-vic-tn.jpg', '35000', 1, 'Brown', 2),
+(17, 'Toyota', 'Corolla', 'Good car', '/phpmotors/images/no-image.png', '/phpmotors/images/no-image.png', '1000', 1, 'Blue', 5);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `reviews`
+--
+
+DROP TABLE IF EXISTS `reviews`;
+CREATE TABLE `reviews` (
+  `reviewId` int(11) NOT NULL,
+  `reviewText` text CHARACTER SET latin1 NOT NULL,
+  `reviewDate` timestamp NOT NULL DEFAULT current_timestamp(),
+  `invId` int(11) NOT NULL,
+  `clientId` int(10) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `reviews`
+--
+
+INSERT INTO `reviews` (`reviewId`, `reviewText`, `reviewDate`, `invId`, `clientId`) VALUES
+(1, 'Awesome with high speed', '2021-07-16 12:42:57', 3, 2),
+(2, 'Good speed', '2021-07-16 12:55:43', 1, 2);
 
 --
 -- Índices para tablas volcadas
@@ -161,6 +227,14 @@ ALTER TABLE `inventory`
   ADD KEY `classificationId` (`classificationId`);
 
 --
+-- Indices de la tabla `reviews`
+--
+ALTER TABLE `reviews`
+  ADD PRIMARY KEY (`reviewId`),
+  ADD KEY `FK_reviews_clients` (`clientId`),
+  ADD KEY `FK_reviews_inventory` (`invId`);
+
+--
 -- AUTO_INCREMENT de las tablas volcadas
 --
 
@@ -168,29 +242,31 @@ ALTER TABLE `inventory`
 -- AUTO_INCREMENT de la tabla `carclassification`
 --
 ALTER TABLE `carclassification`
-  MODIFY `classificationId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `classificationId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT de la tabla `clients`
 --
 ALTER TABLE `clients`
-  MODIFY `clientId` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `clientId` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT de la tabla `images`
 --
 ALTER TABLE `images`
-  MODIFY `imgId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
-
-INSERT INTO `clients` (`clientId`, `clientFirstname`,`clientLastname`,`clientEmail`,`clientPassword`,`clientLevel` ) VALUES
-(1, 'Admin','Administrator','admin@cse340.net','$2y$10$hg9EhPlL1GmyoE8TdU6CMOLBsBdRIcB5F/huEZ6sfgi1D2unZUAFq','3'),
-(2, 'Adam','Hansen','adam@test.com','$2y$10$HZUr1XW7NeCKSdHKLjrLMeGWbvqDrmQ7xdPmi9d5OdCDwoBX9uqFC','1'); 
+  MODIFY `imgId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=55;
 
 --
 -- AUTO_INCREMENT de la tabla `inventory`
 --
 ALTER TABLE `inventory`
-  MODIFY `invId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `invId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+
+--
+-- AUTO_INCREMENT de la tabla `reviews`
+--
+ALTER TABLE `reviews`
+  MODIFY `reviewId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- Restricciones para tablas volcadas
@@ -207,6 +283,13 @@ ALTER TABLE `images`
 --
 ALTER TABLE `inventory`
   ADD CONSTRAINT `inventory_ibfk_1` FOREIGN KEY (`classificationId`) REFERENCES `carclassification` (`classificationId`);
+
+--
+-- Filtros para la tabla `reviews`
+--
+ALTER TABLE `reviews`
+  ADD CONSTRAINT `FK_reviews_clients` FOREIGN KEY (`clientId`) REFERENCES `clients` (`clientId`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `FK_reviews_inventory` FOREIGN KEY (`invId`) REFERENCES `inventory` (`invId`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
